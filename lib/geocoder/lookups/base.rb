556afd813b2998b46bba717a784b7a4d24d0ff71
Hugo Freire <hfreire@abajar.com>
2015-05-07 16:25:50 +0200

    Added thread and datetime log

--- lib/geocoder/lookups/base.rb
+++ lib/geocoder/lookups/base.rb
@@ -209,11 +209,12 @@ module Geocoder
       def make_api_request(query)
         timeout(configuration.timeout) do
           uri = URI.parse(query_url(query))
-          warn uri
+          warn "#{Time.now}:#{Thread.current.object_id}>: #{uri}"
           client = http_client.new(uri.host, uri.port)
           client.use_ssl = true if configuration.use_https
           response = client.get(uri.request_uri, configuration.http_headers)
           warn response.body
+          warn "<#{Time.now}"
           response
         end
       end
